e35fdf717c0ca9e5b307573864cb60f5a2cd1792
Anne Elisabeth <anne.eckersberg@gmail.com>
2018-07-24 16:10:07 +0200

    Add multiplying effect on cell, start work on timing animation

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
-    $("#click").click(function(){ 
-        $(".cell").css({ transform: 'scale(.1)' });
-        $(".cell").clone().appendTo("#cell-clone").animate({left: "200px"});
+        //$(".cell").fadeOut(3000);
     });
-});
+
+    
+    setInterval(
+    function makeDiv() {
+        var divsize = 50;
+        var posx = (Math.random() * ($(".tissue-container").width() - divsize)).toFixed();
+        var posy = (Math.random() * ($(".tissue-container").height() - divsize)).toFixed();
+        $newdiv = $("<div class=cell-clone></div>").css({
+            'left': posx + 'px',
+            'top': posy + 'px'
+        });
+    
+        $newdiv.appendTo('.tissue-container').delay(100).fadeIn(50, function () {           
+            makeDiv();
+            setTimeout(function() {
+                $newdiv.remove();
+            }, 10000);             
+        });
+  
+    }, 10000);
 
